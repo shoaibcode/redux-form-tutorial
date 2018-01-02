7a7f8761f7bc7a94076093a91ce794b4fdb5dcf0
shoaibbhimani <shoaibbhimani1392@gmail.com>
2018-01-02 23:07:52 +0530

    get Values from forma and display it

--- src/container/App.js
+++ src/container/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { formValueSelector } from "redux-form"
+
 
 import logo from "../logo.svg";
 import reduxlogo from "../redux.svg";
@@ -9,19 +11,26 @@ import Login from "./Login"
 
 class App extends Component {
   render() {
+    const {  email, password } = this.props;
     return (
       <div className="App">
         <div className="App-header">
           <Login />
+          <div>Email: {email} </div>
+          <div>password: {password} </div>
         </div>
       </div>
     );
   }
 }
 
+var selector = formValueSelector('login');
+
 const mapStateToProps = state => {
   return {
-    todos: state.todos
+    todos: state.todos,
+    email: selector(state, 'email'),
+    password: selector(state, 'password')
   };
 };
 
